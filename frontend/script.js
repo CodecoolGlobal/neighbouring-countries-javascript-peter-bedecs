6ed5b26ca0bfc83d5bd87038e0814c7fed9218b5
unknown <peter.jung.codecool@gmail.com>
2022-10-24 15:25:37 +0200

    Add script.js and index.html second task in progress

--- frontend/script.js
+++ frontend/script.js
@@ -9,3 +9,16 @@ for (let i = 0; i < countries.length; i++) {
         selectCountry.appendChild(option);
     }
 
+const countryDetails = document.querySelector('#country');
+selectCountry.addEventListener('change', printDetails)
+function printDetails() {
+  countryDetails.innerHTML='';
+  for (let i = 0; i < countries.length; i++) {
+    if(selectCountry.selectedIndex - 1 === i) {
+      const name = document.createElement('h1');
+      name.innerText = countries[i].name.common;
+      countryDetails.appendChild(name);
+    }
+  }
+}
+  console.log(countries.indexOf());

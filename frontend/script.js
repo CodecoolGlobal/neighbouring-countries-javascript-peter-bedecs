23f2230abf4177961542f4c475035073c1e8b4e0
unknown <peter.jung.codecool@gmail.com>
2022-10-27 16:58:10 +0200

    Add scrip.js, style.css and index.html final version

--- frontend/script.js
+++ frontend/script.js
@@ -9,89 +9,38 @@ const toolbar = document.querySelector('#toolbar');
 const prevButton = document.createElement('button');
 prevButton.innerText = 'Previous country';
 toolbar.appendChild(prevButton);
+prevButton.id ='prev';
 prevButton.disabled = true;
 
 
 const nextButton = document.createElement('button');
 nextButton.innerText = 'Next country';
 toolbar.appendChild(nextButton);
+nextButton.id = 'next';
 nextButton.disabled = true;
 
 
+
 prevButton.addEventListener('click', jumpToPrev);
-function jumpToPrev(){
-countryDetails.innerHTML = '';
-let j  = historyCounter - 2;
-let i = historyArray[j] - 1;
-selectCountry.selectedIndex = historyArray[historyCounter - 2];
-historyCounter--;
-if (historyCounter < 2){
-    prevButton.disabled = true;
-} else {
-    prevButton.disabled = false;
-}
-if (historyCounter === historyArray.length){
-    nextButton.disabled = true;
-} else {
-    nextButton.disabled = false;
-}
-let flag = document.createElement('img');
-flag.src = countries[i].flags.png;
-countryDetails.appendChild(flag);
-
-let nameCountry = document.createElement('h1');
-nameCountry.innerText = countries[i].name.common;
-countryDetails.appendChild(nameCountry);
-
-let region = document.createElement('h2');
-region.innerText = countries[i].region;
-countryDetails.appendChild(region);
-
-let subRegion = document.createElement('h3');
-subRegion.innerText = countries[i].subregion;
-countryDetails.appendChild(subRegion);
-
-let capitalCity = document.createElement('h4');
-capitalCity.innerText = countries[i].capital;
-countryDetails.appendChild(capitalCity);
+function jumpToPrev() {
+    countryDetails.innerHTML = '';
+    let j = historyCounter - 2;
+    let i = historyArray[j] - 1;
+    selectCountry.selectedIndex = historyArray[historyCounter - 2];
+    historyCounter--;
+    prevNextButtonSwitch();
+    printCountyDetails(i);
 }
 
 nextButton.addEventListener('click', jumpToNext);
-function jumpToNext(){
+function jumpToNext() {
     countryDetails.innerHTML = '';
-let j = historyCounter;
-let i = historyArray[j] - 1;
-selectCountry.selectedIndex = historyArray[historyCounter];
-historyCounter++;
-if (historyCounter < 2){
-    prevButton.disabled = true;
-} else {
-    prevButton.disabled = false;
-}
-if (historyCounter === historyArray.length){
-    nextButton.disabled = true;
-} else {
-    nextButton.disabled = false;
-}
-let flag = document.createElement('img');
-flag.src = countries[i].flags.png;
-countryDetails.appendChild(flag);
-
-let nameCountry = document.createElement('h1');
-nameCountry.innerText = countries[i].name.common;
-countryDetails.appendChild(nameCountry);
-
-let region = document.createElement('h2');
-region.innerText = countries[i].region;
-countryDetails.appendChild(region);
-
-let subRegion = document.createElement('h3');
-subRegion.innerText = countries[i].subregion;
-countryDetails.appendChild(subRegion);
-
-let capitalCity = document.createElement('h4');
-capitalCity.innerText = countries[i].capital;
-countryDetails.appendChild(capitalCity);
+    let j = historyCounter;
+    let i = historyArray[j] - 1;
+    selectCountry.selectedIndex = historyArray[historyCounter];
+    historyCounter++;
+    prevNextButtonSwitch();
+    printCountyDetails(i);
 }
 
 for (let i = 0; i < countries.length; i++) {
@@ -99,6 +48,11 @@ for (let i = 0; i < countries.length; i++) {
     option.innerText = countries[i].name.common;
     selectCountry.appendChild(option);
 }
+
+const largestPop = document.querySelector('#population');
+largestPop.disabled = true;
+const largestArea = document.querySelector('#area');
+largestArea.disabled = true;
 
 
 const countryDetails = document.querySelector('#country');
@@ -111,40 +65,18 @@ function printDetails() {
     let selectedCountryBorder;
     for (let i = 0; i < countries.length; i++) {
         if (selectCountry.selectedIndex - 1 === i) {
-            let flag = document.createElement('img');
-            flag.src = countries[i].flags.png;
-            countryDetails.appendChild(flag);
-
-            let nameCountry = document.createElement('h1');
-            nameCountry.innerText = countries[i].name.common;
-            countryDetails.appendChild(nameCountry);
-
-            let region = document.createElement('h2');
-            region.innerText = countries[i].region;
-            countryDetails.appendChild(region);
-
-            let subRegion = document.createElement('h3');
-            subRegion.innerText = countries[i].subregion;
-            countryDetails.appendChild(subRegion);
-
-            let capitalCity = document.createElement('h4');
-            capitalCity.innerText = countries[i].capital;
-            countryDetails.appendChild(capitalCity);
+            printCountyDetails(i);
 
             selectedCountryBorder = countries[i].borders;
         }
-        if (historyCounter < 2){
-            prevButton.disabled = true;
-        } else {
-            prevButton.disabled = false;
-        }
+        prevNextButtonSwitch()
     }
 
     const largestPop = document.querySelector('#population');
     largestPop.addEventListener('click', getLargestPop);
 
     function getLargestPop() {
-               let maxPop = 0;
+        let maxPop = 0;
         let maxCountry = '';
         for (let i = 0; i < countries.length; i++) {
             if (selectedCountryBorder.includes(countries[i].cca3)) {
@@ -154,39 +86,17 @@ function printDetails() {
                 }
             }
         }
-        
+
         countryDetails.innerHTML = '';
-        for (let j = 1; j < countries.length; j++) {
-            if (countries[j].name.common === maxCountry) {
-                selectCountry.selectedIndex = j + 1;
-                let flag = document.createElement('img');
-                flag.src = countries[j].flags.png;
-                countryDetails.appendChild(flag);
-
-                let nameCountry = document.createElement('h1');
-                nameCountry.innerText = countries[j].name.common;
-                countryDetails.appendChild(nameCountry);
-
-                let region = document.createElement('h2');
-                region.innerText = countries[j].region;
-                countryDetails.appendChild(region);
-
-                let subRegion = document.createElement('h3');
-                subRegion.innerText = countries[j].subregion;
-                countryDetails.appendChild(subRegion);
-
-                let capitalCity = document.createElement('h4');
-                capitalCity.innerText = countries[j].capital;
-                countryDetails.appendChild(capitalCity);   
+        for (let i = 1; i < countries.length; i++) {
+            if (countries[i].name.common === maxCountry) {
+                selectCountry.selectedIndex = i + 1;
+                printCountyDetails(i);
             }
         }
         historyArray.push(selectCountry.selectedIndex);
         historyCounter++;
-        if (historyCounter < 2){
-            prevButton.disabled = true;
-        } else {
-            prevButton.disabled = false;
-        }
+        prevNextButtonSwitch()
     }
 
     const largestArea = document.querySelector('#area');
@@ -205,37 +115,60 @@ function printDetails() {
             }
         }
         countryDetails.innerHTML = '';
-        for (let j = 1; j < countries.length; j++) {
-            if (countries[j].name.common === maxCountry) {
-                selectCountry.selectedIndex = j + 1;
-                let flag = document.createElement('img');
-                flag.src = countries[j].flags.png;
-                countryDetails.appendChild(flag);
-
-                let nameCountry = document.createElement('h1');
-                nameCountry.innerText = countries[j].name.common;
-                countryDetails.appendChild(nameCountry);
-
-                let region = document.createElement('h2');
-                region.innerText = countries[j].region;
-                countryDetails.appendChild(region);
-
-                let subRegion = document.createElement('h3');
-                subRegion.innerText = countries[j].subregion;
-                countryDetails.appendChild(subRegion);
-
-                let capitalCity = document.createElement('h4');
-                capitalCity.innerText = countries[j].capital;
-                countryDetails.appendChild(capitalCity);
+        for (let i = 1; i < countries.length; i++) {
+            if (countries[i].name.common === maxCountry) {
+                selectCountry.selectedIndex = i + 1;
+                printCountyDetails(i);
             }
         }
         historyArray.push(selectCountry.selectedIndex);
         historyCounter++;
-        if (historyCounter < 2){
-            prevButton.disabled = true;
-        } else {
-            prevButton.disabled = false;
-        }
+        prevNextButtonSwitch()
     }
+    if (typeof selectedCountryBorder === 'undefined'){
+        largestPop.innerText = 'No neighbour';
+        largestPop.disabled = true;
+        largestArea.innerText = 'No neighbour';
+        largestArea.disabled = true;
+      } else {
+        largestPop.innerText = 'Neighbour with the largest population';
+        largestPop.disabled = false;
+        largestArea.innerText = 'Neighbour with the largest area';
+        largestArea.disabled = false;
+      }
+}
 
+function prevNextButtonSwitch() {
+    if (historyCounter < 2) {
+        prevButton.disabled = true;
+    } else {
+        prevButton.disabled = false;
+    }
+    if (historyCounter === historyArray.length) {
+        nextButton.disabled = true;
+    } else {
+        nextButton.disabled = false;
+    }
+}
+
+function printCountyDetails(i) {
+    let flag = document.createElement('img');
+    flag.src = countries[i].flags.png;
+    countryDetails.appendChild(flag);
+
+    let nameCountry = document.createElement('h1');
+    nameCountry.innerText = countries[i].name.common;
+    countryDetails.appendChild(nameCountry);
+
+    let region = document.createElement('h2');
+    region.innerText = countries[i].region;
+    countryDetails.appendChild(region);
+
+    let subRegion = document.createElement('h3');
+    subRegion.innerText = countries[i].subregion;
+    countryDetails.appendChild(subRegion);
+
+    let capitalCity = document.createElement('h4');
+    capitalCity.innerText = countries[i].capital;
+    countryDetails.appendChild(capitalCity);
 }
